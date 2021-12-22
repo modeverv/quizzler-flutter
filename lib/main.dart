f7d88a66e4aed4af73026f77b794dc951d28c05a
seijiro <modeverv@gmail.com>
2021-12-22 20:33:19 +0900

    2021-12-22 20:33:18

--- lib/main.dart
+++ lib/main.dart
@@ -31,6 +31,12 @@ class _QuizPageState extends State<QuizPage> {
   }
 
   List<Widget> scoreKeeper = [];
+  List<String> questions = [
+    'You can lead a cow down stairs but not up stairs.',
+    'Approximately one quarter of human bones are in the feet.',
+    'A slug\'s blood is green.',
+  ];
+  int questionNumber = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -44,7 +50,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[questionNumber],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -71,6 +77,8 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked true.
                 setState(() {
                   scoreKeeper.add(buildRowColumnWidget(true));
+                  questionNumber++;
+                  questionNumber = questionNumber % questions.length;
                 });
               },
             ),
@@ -92,6 +100,8 @@ class _QuizPageState extends State<QuizPage> {
                 //The user picked false.
                 setState(() {
                   scoreKeeper.add(buildRowColumnWidget(false));
+                  questionNumber++;
+                  questionNumber = questionNumber % questions.length;
                 });
               },
             ),
